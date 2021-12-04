778248c08623c370a2d13e9424d74918ad637111
Theresemyl <tleung912@gmail.com>
2021-12-03 17:45:38 -0800

    Add conditional to matches page to show correct user

--- client/src/pages/Matches/Matches.js
+++ client/src/pages/Matches/Matches.js
@@ -1,6 +1,12 @@
-//must be logged in to see this page
+// must be logged in to see this page
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
+// relative time format
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en.json";
+import ReactTimeAgo from "react-time-ago";
+TimeAgo.addDefaultLocale(en);
 
 function Matches({ userName, userId }) {
   const [likedRestaurants, setLikedRestaurants] = useState([]);
@@ -19,7 +25,6 @@ function Matches({ userName, userId }) {
           }
         });
         setLikedRestaurants(likedList);
-        console.log(likedList);
       })
       .catch((err) => {
         console.log(err);
@@ -32,15 +37,31 @@ function Matches({ userName, userId }) {
       <h1 className="dashboard__title">Matches page</h1>
       <p>Welcome to your matches!</p>
 
-      <p>Here are your past likes:</p>
+      <p>Here are your past matches:</p>
+
       {likedRestaurants.map((like) => (
         <div>
-          <p>
-            <b>{like.name}</b> <br />
-            {like.address}
-            <br />
-            You and <b>{like.matched_user_name1}</b> matched on {like.date}!
-          </p>
+          {like.matched_user_name1 === userName ? (
+            <div>
+              <p>
+                <b>{like.name}</b> <br />
+                {like.address}
+                <br />
+                You and <b>{like.matched_user_name2}</b> matched <br />
+                <ReactTimeAgo date={Number(like.date)} locale="en-US" />!
+              </p>
+            </div>
+          ) : (
+            <div>
+              <p>
+                <b>{like.name}</b> <br />
+                {like.address}
+                <br />
+                You and <b>{like.matched_user_name1}</b> matched <br />
+                <ReactTimeAgo date={Number(like.date)} locale="en-US" />!
+              </p>
+            </div>
+          )}
         </div>
       ))}
     </main>
