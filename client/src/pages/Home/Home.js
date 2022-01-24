38efe7f611bcf2d0252dfc33d37d530719daf9af
Theresemyl <tleung912@gmail.com>
2022-01-23 21:46:23 -0800

    Add temporary close down message on home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -11,6 +11,14 @@ function Home() {
           <h1>🍽️</h1>
           <h1>PlaceMatch</h1>
           <p>Click to begin</p>
+          <br />
+          <br />
+          <br />
+          <p>
+            ** Attn: App is currently temporarily suspended due to account
+            expiration on the Google Cloud & Maps Platform. A solution is in
+            progress! **
+          </p>
         </div>
       </Link>
     </>
